d35dd9d7a2e0043a7485c112df76d2d601a8a480
Hassan El Abdallah <74018203+hsnkh12@users.noreply.github.com>
2025-03-29 23:06:26 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
         // Create a new database
         string dbName = "TestDB";
         string dbPassword = "SecurePass123";
-        var volumePath = "C:\path\to\your\db\folder\";
+        var volumePath = @"C:\path\to\your\db\folder\";
         JsonDatabase db = new JsonDatabase(dbName, dbPassword, volumePath);
 
         // Synchronize database with the disk
